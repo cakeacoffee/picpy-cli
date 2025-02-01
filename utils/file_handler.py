7a4c31fa7d101669e0fa61af408efdfbd5d05620
Stella Bischoff <bischoff.stella3@gmail.com>
2025-02-01 16:24:45 +0000

    file_handler outline

---
+++ utils/file_handler.py
@@ -0,0 +1,47 @@
+import os
+
+
+def process(
+    input_path: str = "images/input", output_path: str = "images/output"
+) -> None:
+    for filename in os.listdir(input_path):
+        print(filename)
+
+
+# **--- FILE HANDLING ---**
+
+
+def copy_files(
+    input_path: str = "images/input", output_path: str = "images/output"
+) -> None:
+    """copy files from input directory to output directory"""
+    # TODO: implement (keep name and default, by date, by base name and increment)
+
+
+# **--- FILE DATA ---**
+
+
+def list_files(
+    input_path: str = "images/input",
+) -> None:
+    """list all files in the given directory"""
+    for filename in os.listdir(input_path):
+        print(filename)
+
+
+def count_file_type(
+    input_path: str = "images/input",
+) -> str:
+    """count file type and display and return result"""
+    result = ""
+    # TODO: implement
+    return result
+
+
+def file_size(
+    input_path: str = "images/input",
+) -> str:
+    """get size for each file and display and return result"""
+    result = ""
+    # TODO: implement
+    return result
