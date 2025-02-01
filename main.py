48f431a53d64846e90581aa3d771334524a58d49
Stella Bischoff <bischoff.stella3@gmail.com>
2025-02-01 16:54:42 +0000

    added to do

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import file_handler
+from utils import format_handler
 
 if __name__ == "__main__":
     file_handler.process()
---
+++ utils/format_handler.py
@@ -0,0 +1,10 @@
+import os
+import numpy
+import cv2 as cv
+
+
+def path_to_image(input_path: str) ->  numpy.ndarray | None:
+    """take an path to a file and create numpy array"""
+    return cv.imread(input_path)
+
+
